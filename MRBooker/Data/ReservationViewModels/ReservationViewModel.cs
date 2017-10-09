86dbb93ea92a8ada67e14d3bd5b40a7a0f7508dc
Tibi Teglas <tteglas@siteworx.com>
2017-10-09 17:04:43 +0300

    Changed some models. Master commit

---
+++ MRBooker/Data/ReservationViewModels/ReservationViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace MRBooker.Data.ReservationViewModels
+{
+    public class ReservationViewModel
+    {
+        public ICollection<string> Reservations { get; set; }
+
+        public string JsonList { get; set; }
+
+        public string ToJsonList()
+        {
+            if (Reservations == null)
+            {
+                return string.Empty;
+            }
+            else
+            {
+                return JsonConvert.SerializeObject(Reservations);
+            }
+        }
+    }
+}
